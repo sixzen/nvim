6005070f9d70abc22ab7df09d179ad3dfc2670e4
Angelo <angelo.zapatalillo@gmail.com>
2024-12-16 10:56:03 -0300

    feat: transparent, add avante

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -17,6 +17,35 @@ return {
     end,
   },
   {
+    "eldritch-theme/eldritch.nvim",
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("eldritch").setup {
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark",   -- style for floating windows
+        },
+
+        on_colors = function(colors)
+          colors.bg = colors.sidebars
+        end,
+        -- on_highlights = function(highlights, colors)
+        --   highlights.StatusLine = { fg = colors.white, bg = colors.sidebars }
+        -- end,
+      }
+      vim.cmd.colorscheme "eldritch"
+    end,
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
@@ -29,7 +58,7 @@ return {
           light = "latte",
           dark = "mocha",
         },
-        transparent_background = false,
+        transparent_background = true,
         show_end_of_buffer = false, -- show the '~' characters after the end of buffers
         term_colors = false,
         dim_inactive = {
@@ -58,15 +87,41 @@ return {
           return {
             StatusLine = { bg = colors.none },
             TabLineFill = { bg = colors.none },
+
             TreesitterContextBottom = {
               sp = colors.surface0,
               style = {},
             },
+            TreesitterContext = { bg = colors.mantle },
+            TreesitterContextLineNumber = { fg = colors.surface1, bg = colors.mantle },
+
+            NormalFloat = { bg = colors.mantle },
+
             PackageInfoOutdatedVersion = { fg = colors.peach },
             CmpNormal = { bg = colors.mantle },
             LspInlayHint = { fg = colors.surface1, bg = colors.none },
-            -- InclineNormal = { bg = colors.mantle },
-            -- InclineNormalNC = { bg = colors.surface0 },
+            InclineNormal = { bg = colors.mantle },
+            InclineNormalNC = { bg = colors.mantle },
+
+            TelescopeBorder = { fg = colors.base, bg = colors.mantle },
+            TelescopeNormal = { bg = colors.mantle },
+            TelescopePreviewBorder = { fg = colors.mantle, bg = colors.mantle },
+            TelescopePreviewNormal = { bg = colors.mantle },
+            TelescopePreviewTitle = { fg = colors.mantle, bg = colors.green },
+            TelescopePromptBorder = { fg = colors.base, bg = colors.base },
+            TelescopePromptNormal = { fg = colors.text, bg = colors.base },
+            TelescopePromptPrefix = { fg = colors.red, bg = colors.base },
+            TelescopePromptTitle = { fg = colors.mantle, bg = colors.red },
+            TelescopeResultsBorder = { fg = colors.mantle, bg = colors.mantle },
+            TelescopeResultsNormal = { bg = colors.mantle },
+            TelescopeResultsTitle = { fg = colors.mantle, bg = colors.mantle },
+
+            NeoTreeFloatBorder = { fg = colors.mantle, bg = colors.mantle },
+            NeoTreeFloatTitle = { fg = colors.mantle, bg = colors.red },
+            NeoTreeNormal = { bg = colors.mantle },
+
+            FloatHover = { bg = colors.mantle },
+            FloatBorderHover = { fg = colors.mantle, bg = colors.mantle },
           }
         end,
         integrations = {
