cb6c3589d029bed8659eaea5c86b0cb473f437c4
Angelo <angelo.zapatalillo@gmail.com>
2025-11-05 01:21:30 -0300

    feat: add copilot and testing sidekick with opencode

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,24 +1,51 @@
 return {
   {
     "zbirenbaum/copilot.lua",
-    -- event = "BufRead",
-    -- dependencies = {
-    --   {
-    --     "zbirenbaum/copilot-cmp",
-    --     opts = {
-    --       formatters = {
-    --         insert_text = require("copilot_cmp.format").remove_existing,
-    --       },
-    --     },
-    --   },
-    -- },
+    event = "BufReadPost",
+    dependencies = {
+      -- "copilotlsp-nvim/copilot-lsp",
+      -- init = function()
+      --   vim.g.copilot_nes_debounce = 500
+      -- end,
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "BlinkCmpMenuOpen",
+        callback = function()
+          vim.b.copilot_suggestion_hidden = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "BlinkCmpMenuClose",
+        callback = function()
+          vim.b.copilot_suggestion_hidden = false
+        end,
+      })
+    end,
     opts = {
       panel = {
         enabled = false,
       },
       suggestion = {
-        enabled = false,
+        enabled = true,
+        auto_trigger = false,
+        hide_during_completion = true,
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
       },
+      should_attach = function(_, bufname)
+        if string.match(bufname, "env") then
+          return false
+        end
+
+        return true
+      end,
     },
   },
   -- {
