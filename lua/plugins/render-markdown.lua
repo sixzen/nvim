b9538af29b74540f62c5036ada4673c04d0bf782
Angelo <angelo.zapatalillo@gmail.com>
2026-01-13 15:57:21 -0300

    feat: delete nvim web devicons

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -13,5 +13,5 @@ return {
       },
     },
   },
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
 }
