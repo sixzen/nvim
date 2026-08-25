0cb3bef6d23c8400bc5ad36b6d50293b676d18fe
sixzen <angelo.zapatalillo@gmail.com>
2026-08-25 16:28:27 -0400

    feat: change oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -9,5 +9,36 @@ return {
       desc = "Start oil",
     },
   },
-  opts = {},
+  opts = {
+    default_file_explorer = true,
+    columns = {
+      "icon"
+    },
+    buf_options = {
+      buflisted = false,
+      bufhidden = "hide",
+    },
+    delete_to_trash = false,
+    skip_confirm_for_simple_edits = true,
+    prompt_save_on_select_new_entry = true,
+    cleanup_delay_ms = 2000,
+    lsp_file_methods = {
+      enabled = true,
+      timeout_ms = 1000,
+      autosave_changes = false,
+    },
+    constrain_cursor = "editable",
+    watch_for_changes = false,
+    use_default_keymaps = true,
+    view_options = {
+      show_hidden = true,
+      natural_order = "fast",
+      case_insensitive = false,
+    },
+    preview_win = {
+      update_on_cursor_moved = true,
+      -- How to open the preview window "load"|"scratch"|"fast_scratch"
+      preview_method = "fast_scratch",
+    },
+  },
 }
